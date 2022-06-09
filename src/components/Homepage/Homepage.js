a9b66ed21603b1b7870f4ae59844d3616899ddf3
Keyur Shah <75299295+keyurshah339@users.noreply.github.com>
2022-06-09 15:45:56 +0530

    features

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,30 +1,30 @@
-import "./Homepage.css";
-import { Link } from "react-router-dom";
-import { useAuth } from "../Reducer/AuthReducer";
-export function Homepage() {
-  const { stateAuth } = useAuth();
-  const { isUserAuthenticated } = stateAuth;
-  return isUserAuthenticated ? (
-    <div className="homepage">
-      <p>TO TRAVEL IS TO LIVE</p>
-      <div className="homepage-buttons-outer">
-        <Link to="/explore">
-          <button className="homepage-single-button">Explore</button>
-        </Link>
-      </div>
-    </div>
-  ) : (
-    <div className="homepage">
-      <p>TO TRAVEL IS TO LIVE</p>
-    
-      <div className="homepage-buttons-outer">
-        <Link to="/explore">
-          <button className="homepage-single-button">Explore</button>
-        </Link>
-        <Link to="/login">
-          <button className="homepage-single-button">Sign In</button>
-        </Link>
-      </div>
-    </div>
-  );
-}
+    import "./Homepage.css";
+    import { Link } from "react-router-dom";
+    import { useAuth } from "../Reducer/AuthReducer";
+    export function Homepage() {
+      const { stateAuth } = useAuth();
+      const { isUserAuthenticated } = stateAuth;
+      return isUserAuthenticated ? (
+        <div className="homepage">
+          <p>TO TRAVEL IS TO LIVE</p>
+          <div className="homepage-buttons-outer">
+            <Link to="/explore">
+              <button className="homepage-single-button">Explore</button>
+            </Link>
+          </div>
+        </div>
+      ) : (
+        <div className="homepage">
+          <p>TO TRAVEL IS TO LIVE</p>
+        
+          <div className="homepage-buttons-outer">
+            <Link to="/explore">
+              <button className="homepage-single-button">Explore</button>
+            </Link>
+            <Link to="/login">
+              <button className="homepage-single-button">Sign In</button>
+            </Link>
+          </div>
+        </div>
+      );
+    }
